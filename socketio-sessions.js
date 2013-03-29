5ba0ed6e9a4ecfa2d1bee214d091d13f11899bf4
Henry Merriam <henry@zephyrhosting.net>
2013-03-29 13:37:33 -0400

    Cleaner control flow

--- socketio-sessions.js
+++ socketio-sessions.js
@@ -18,28 +18,14 @@ var connect = require('connect')
  */
 
 exports.load = function (handshake, store, key, secret, fn) {
-	if (handshake.headers.cookie) {
-		var parsed = cookie.parse(handshake.headers.cookie);
-		if (key in parsed) {
-			var sid = connect.utils.parseSignedCookie(parsed[key], secret);
-			if (sid) {
-				store.load(sid, function (err, sess) {
-					if (err) {
-						fn(err);
-					} else {
-						if (sess) {
-							sess.touch();
-						}
-						fn(err, sess);
-					}
-				});
-			} else {
-				fn(new Error("Session key signature doesn't match secret key."));
-			}
-		} else {
-			fn(null, null);
-		}
-	} else {
-		fn(null, null);
-	}
+	if (!handshake.headers.cookie) { return fn(null, null); }
+	var parsed = cookie.parse(handshake.headers.cookie);
+	if (!(key in parsed)) { return fn(null, null); }
+	var sid = connect.utils.parseSignedCookie(parsed[key], secret);
+	if (!sid) { return fn(new Error("Session key signature doesn't match secret key.")); }
+	store.load(sid, function (err, sess) {
+		if (err) { return fn(err); }
+		if (sess) { sess.touch(); }
+		return fn(err, sess);
+	});
 };
